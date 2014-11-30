dabf70bccf41851d936cf0080c279685a6015e6a
Piotr <piotr@pc17.home>
2014-11-30 02:14:29 +0100

    fixed simulator.m

--- simulator.m
+++ simulator.m
@@ -1,22 +1,38 @@
-wezel = struct();
-wezel.zadanie = 0;
-wezel.faza = 0;
-wezel.czas = 0;
+% Przyk³adowa inicjalizacja informacji o sieci
+clear;
+M1=struct();
+M2=struct();
+M3=struct();
+M4=struct();
+LU=struct();
+
+M1.i=1; M1.o=1;
+M2.i=2; M2.o=1;
+M3.i=1; M3.o=3;
+M4.i=4; M4.o=4;
+LU.i=1; LU.o=1;
+
+machinesSize=[M1 M2 M3 M4 LU];
+agvsNumber=1;
+
+%inicjalizacja symulatora
+[machines, agvs] = simInit(machinesSize, agvsNumber);
+machines=vertcat(machines.machine); %inaczej jest problem z iterowaniem 
+%uruchomienie zadania na maszynie
+
+machines(1)= loadOnInputBuf( machines(1), 1, 3);
+
+%³adowanie z bufora do maszyny
+machines(1)= loadOnMachineSocket(machines(1), 3);
+
+%³adowania z maszyny na wózek
+%agv= loadOnAgv(machines(1), 3);
+
+
+%przejazd z a do b
 
 
 
-maszyna = struct();
-maszyna.buforWe = wezel;
-maszyna.gniazdo = wezel;
-maszyna.buforWy = wezel;
 
-
-AGV = struct();
-AGV.segment = 0;
-AGV.czasOdjazdu = 0;
-AGV.maszyna = maszyna;
-
-zakonczone.listMaszyn  = [maszyna maszyna maszyna];
-zakonczone.listaAGV = [AGV AGV];
 
 
